7e48bf57aab4df6386c85ceda9247219cefdb76c
Geoffrey00 <91313655+Geoffrey00@users.noreply.github.com>
2023-03-28 17:11:43 +0200

    feat: redirect to login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, useNavigate  } from "react-router-dom";
 import Home from './Page/Home/Home'
 import Login from './Page/Login/LoginPage'
 
 export function RequireAuth({ children }) {
     // Used to ensure the refreshToken is called once at a time
-    const user = 'ok'; // TODO Get user from local storage
+    const user = null
+    const navigate = useNavigate();
 
     if (user === null) {
-        //TODO Navigate to login
+        return <Navigate to="/login" replace={true} />
     } else {
         return children;
     }
@@ -21,7 +22,11 @@ function App() {
       //TODO ROUTER
       <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={
+            <RequireAuth>
+                <Home />
+            </RequireAuth>
+        } />
         <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,8 +1,8 @@
-
+import { useEffect } from "react";
+import { Navigate, useNavigate  } from "react-router-dom";
+    
 
 const HomePage = () => {
-
-
     return(
         <div>
             Home page
