9528c75b0fc1a9e5c59eff00a460ad021625fdd0
Geoffrey00 <91313655+Geoffrey00@users.noreply.github.com>
2023-05-16 15:37:21 +0200

    feat/listing: display data

--- src/Hook/Query/useGetData.js
+++ src/Hook/Query/useGetData.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
 
-const getData = () => {
+const getData = async (setData) => {
 
     const token = window.localStorage.getItem('token')
 
     axios
         .get('http://127.0.0.1:8000/api/cats', {
             headers: {
-                'Authorization' : 'Bearer ' .concat(token),
+                'Authorization' : 'Bearer ' + token,
             }        
         }
         )
         .then(function (response) {
-            console.log(response);
+            setData(response)
         });
 }
 
--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Navigate, useNavigate  } from "react-router-dom";
 import getData from "../../Hook/Query/useGetData";
     
@@ -7,13 +7,56 @@ const HomePage = () => {
 
     const token = window.localStorage.getItem('token')
 
+    const [data, setData] = useState(null);  
+
+    const getDataFunction = async () => {
+        await getData(setData) 
+        console.log(data);
+   }
+
+ 
     useEffect(() => {
-        getData()
-    })
+
+       getDataFunction()
+        
+    }, [])
+
+    const style = {
+        ul: {
+            display:'flex',
+            flexWrap:'wrap'
+        },
+        li:{
+            listStyle:'none',
+            display:'flex',
+            margin:'50px',
+            textAlign:'center',
+            flexDirection:'column'
+        },
+        img :{
+            maxWidth : '300px',
+            width:'300px',
+            minHeight :'300px',
+            height:'300px',
+            objectFit :'cover'
+        }
+    }
+
 
     return(
-        <div>
-            Home page
+        <div>  
+            <p>Vous êtes connecté !</p>
+            <p>Liste des chats :  </p> 
+            {data ?
+            <ul style={style.ul}>
+                {data.data['hydra:member'].map((item) => 
+                     <li style={style.li} key={item.id}>
+                        <img style={style.img} src={item.image_link} alt="" />
+                        {item.name}                     
+                     </li>
+                )}
+            </ul> : 'Loading'
+            }
         </div>
     )
 }
